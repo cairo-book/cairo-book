da3bcbcacefdeb0242e9fe1a373a154018046410
glihm <dev@glihm.net>
2023-05-16 16:41:50 -0600

    rework summary output for more readability

--- cairo_programs_verifier.sh
+++ cairo_programs_verifier.sh
@@ -7,31 +7,41 @@ rm -rf output/*
 echo "# Cairo program verifier"
 echo ""
 echo "The list of cairo programs below is auto-generated from the markdown sources.  "
-echo "Any code block with the attribute \`file=filename.cairo\` will be extracted as"
-echo "a standalone program and then compiled."
+echo "Any code block with a `fn main()` function will be compiled (except if the attribute `does_not_compile` is manually added)."
+echo ""
 
-echo "| Cairo program | Result | Logs |"
-echo "| :-----------: | :----: | :--- |"
+has_error=false
 
 for prog in *.cairo; do
   cairo-run "$prog" > output/"$prog".out 2> output/"$prog".err
 
   compile_code="$?"
 
-  o="$(cat output/$prog.out)"
-  # remove new lines and extra spaces.
-  o="${o//$'\n'/' '}"
-  o="$(echo $e | sed 's/  \+/ /g')"
+  # Output is not displayed at the moment. Keep it commented for now.
+  # o="$(cat output/$prog.out)"
+  # # remove new lines and extra spaces.
+  # o="${o//$'\n'/' '}"
+  # o="$(echo $e | sed 's/  \+/ /g')"
 
   err="$(cat output/$prog.err)"
-  err2="$(printf '%s' "$err" | sed 's/\\n/<br>/g')"
 
-  if [ $compile_code -eq 0 ]; then
-      echo "| $prog | :heavy_check_mark: |  |"
-  else
-      echo "| $prog | :x: | <pre>$err2</pre> |"
+  if [ $compile_code -ne 0 ]; then
+      has_error=true
+
+      echo ":x: **$prog**"
+      echo "<pre>$err</pre>"
+
+      echo ""
+      echo "---  "
+      echo ""
   fi
 
 done
+
+if [ "$has_error" = false ] ; then
+    echo ":heavy_check_mark: All Cairo programs were compiled successfully."
+else
+    echo ":x: Some Cairo programs have errors, please check the list above."    
+fi
 
 echo ""
