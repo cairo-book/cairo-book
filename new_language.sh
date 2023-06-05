7276c4b722c411a1f15fa168fdd09d0bc2e05a09
cryptonerdcn <cryptonerdcn@gmail.com>
2023-06-05 17:05:11 +0900

    Add a bash for non-developer, who just want to generate a new .po file for translation and don't want to set up the environment.

---
+++ new_language.sh
@@ -0,0 +1,41 @@
+LANG=$1
+FILE=po/$LANG.po
+OS=$(uname)
+
+# Stop if no laguage input.
+if [ $# -eq 0 ]
+then
+    echo ""
+    echo "No input language, exit."
+    exit 0
+fi
+
+#Linux
+if [ "$OS" == "Linux" ]; then
+    ARCH=$(uname -m)
+    # Install gettext
+    sudo apt install gettext
+    # Download mdBook and extract it out
+    if [ "$ARCH" == "aarch64" ]; then
+        curl -L https://github.com/rust-lang/mdBook/releases/download/v0.4.30/mdbook-v0.4.30-aarch64-unknown-linux-musl.tar.gz | tar -xz
+    else
+        curl -L https://github.com/rust-lang/mdBook/releases/download/v0.4.30/mdbook-v0.4.30-x86_64-unknown-linux-musl.tar.gz | tar -xz
+    fi
+# MACOS
+elif [ "$OS" == "Darwin" ]; then
+    # Install gettext
+    brew install gettext
+    # Download mdBook and extract it out
+    curl -L https://github.com/rust-lang/mdBook/releases/download/v0.4.30/mdbook-v0.4.30-x86_64-apple-darwin.tar.gz | tar -xz
+else
+    echo "The operating system is not Linux or macOS."
+    exit 0
+fi
+
+# Build a new LANGUAGES. po file if not exist .
+if test -f "$FILE"; then
+    echo ""
+    echo "File $FILE already exists. Ensure you're building a new book. Alternatively, use 'translations.sh' to serve an existing one."
+else
+    msginit -i po/messages.pot -l $LANG -o po/$LANG.po
+fi
