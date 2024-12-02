7ba9249f830a1639b84513cc80307170cd922267
cryptonerdcn <cryptonerdcn@gmail.com>
2024-12-02 16:22:24 +0900

    Update WASM-Cairo to v0.10.2.

--- theme/pkg/wasm-cairo_bg.wasm.d.ts
+++ theme/pkg/wasm-cairo_bg.wasm.d.ts
@@ -10,3 +10,4 @@ export function __wbindgen_add_to_stack_pointer(a: number): number;
 export function __wbindgen_export_0(a: number, b: number): number;
 export function __wbindgen_export_1(a: number, b: number, c: number, d: number): number;
 export function __wbindgen_export_2(a: number, b: number, c: number): void;
+export function __wbindgen_export_3(a: number): void;
