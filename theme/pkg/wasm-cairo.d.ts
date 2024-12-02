b8dcb1069752e468408793932f2ce82e4c64fa30
cryptonerdcn <cryptonerdcn@gmail.com>
2024-12-02 16:50:54 +0900

    Update WASM-Cairo to v0.10.2. (#1039)

--- theme/pkg/wasm-cairo.d.ts
+++ theme/pkg/wasm-cairo.d.ts
@@ -2,45 +2,45 @@ declare namespace wasm_bindgen {
 	/* tslint:disable */
 	/* eslint-disable */
 	/**
-	* @param {string} s
-	* @returns {string}
-	*/
+	 * @param {string} s
+	 * @returns {string}
+	 */
 	export function greet(s: string): string;
 	/**
-	* @param {string} cairo_program
-	* @param {boolean} replace_ids
-	* @returns {string}
-	*/
+	 * @param {string} cairo_program
+	 * @param {boolean} replace_ids
+	 * @returns {string}
+	 */
 	export function compileCairoProgram(cairo_program: string, replace_ids: boolean): string;
 	/**
-	* @param {string} cairo_program
-	* @param {number | undefined} available_gas
-	* @param {boolean} allow_warnings
-	* @param {boolean} print_full_memory
-	* @param {boolean} run_profiler
-	* @param {boolean} use_dbg_print_hint
-	* @returns {string}
-	*/
+	 * @param {string} cairo_program
+	 * @param {number | undefined} available_gas
+	 * @param {boolean} allow_warnings
+	 * @param {boolean} print_full_memory
+	 * @param {boolean} run_profiler
+	 * @param {boolean} use_dbg_print_hint
+	 * @returns {string}
+	 */
 	export function runCairoProgram(cairo_program: string, available_gas: number | undefined, allow_warnings: boolean, print_full_memory: boolean, run_profiler: boolean, use_dbg_print_hint: boolean): string;
 	/**
-	* @param {string} cairo_program
-	* @param {boolean} allow_warnings
-	* @param {string} filter
-	* @param {boolean} include_ignored
-	* @param {boolean} ignored
-	* @param {boolean} starknet
-	* @param {string} run_profiler
-	* @param {boolean} gas_disabled
-	* @param {boolean} print_resource_usage
-	* @returns {string}
-	*/
+	 * @param {string} cairo_program
+	 * @param {boolean} allow_warnings
+	 * @param {string} filter
+	 * @param {boolean} include_ignored
+	 * @param {boolean} ignored
+	 * @param {boolean} starknet
+	 * @param {string} run_profiler
+	 * @param {boolean} gas_disabled
+	 * @param {boolean} print_resource_usage
+	 * @returns {string}
+	 */
 	export function runTests(cairo_program: string, allow_warnings: boolean, filter: string, include_ignored: boolean, ignored: boolean, starknet: boolean, run_profiler: string, gas_disabled: boolean, print_resource_usage: boolean): string;
 	/**
-	* @param {string} starknet_contract
-	* @param {boolean} allow_warnings
-	* @param {boolean} replace_ids
-	* @returns {string}
-	*/
+	 * @param {string} starknet_contract
+	 * @param {boolean} allow_warnings
+	 * @param {boolean} replace_ids
+	 * @returns {string}
+	 */
 	export function compileStarknetContract(starknet_contract: string, allow_warnings: boolean, replace_ids: boolean): string;
 	
 }
@@ -58,14 +58,15 @@ declare interface InitOutput {
   readonly __wbindgen_export_0: (a: number, b: number) => number;
   readonly __wbindgen_export_1: (a: number, b: number, c: number, d: number) => number;
   readonly __wbindgen_export_2: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_export_3: (a: number) => void;
 }
 
 /**
 * If `module_or_path` is {RequestInfo} or {URL}, makes a request and
 * for everything else, calls `WebAssembly.instantiate` directly.
 *
-* @param {InitInput | Promise<InitInput>} module_or_path
+* @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
 *
 * @returns {Promise<InitOutput>}
 */
-declare function wasm_bindgen (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
+declare function wasm_bindgen (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
